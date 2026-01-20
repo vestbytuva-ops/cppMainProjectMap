1f3eb414997201e5b05fe127ec098991d13f9b15
SimplyHero-dev <eric07.tran@gmail.com>
2026-01-20 11:47:20 +0100

    doubleNumber code

--- index.cpp
+++ index.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+void doubleNumber(){
+    int value {};
+    std::cin >> value ;
+    std::cout << "double that number is: " << value*2 << '\n' ;
+}
+
 int main(){
-    std::cout << "Hello World!";
+    std::cout << "Enter an integer:" << '\n' ;
+    doubleNumber();
+
+    return 0;
 }
