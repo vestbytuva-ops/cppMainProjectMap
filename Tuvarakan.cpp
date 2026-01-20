e08db0479cc343ca6a6b66b43ab287b94755bd08
vestbytuva-ops <vestby.tuva@gmail.com>
2026-01-20 19:39:28 +0100

    New code

--- Tuvarakan.cpp
+++ Tuvarakan.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main (){
- 
+        int a, b;
+
+    cout << "Skriv inn første tall: ";
+    cin >> a;
+
+    cout << "Skriv inn andre tall: ";
+    cin >> b;
+
+    int sum = a + b;
+
+    cout << "Summen er: " << sum << endl;
+
+    return 0;
 }
 
